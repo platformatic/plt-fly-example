15c549d4cec41174f5d88776399dd631f95567dc
MzUgM <108896003+MzUgM@users.noreply.github.com>
2022-11-01 14:13:34 -0700

    First platformatic intialization

---
+++ README.md
@@ -0,0 +1,4 @@
+# Deploy a Platformatic DB Application to Fly.io
+
+This is our demo project which you can duplicate by following our guide at
+https:oss.platformatic.dev 
---
+++ global.d.ts
@@ -0,0 +1,8 @@
+import { Entity } from '@platformatic/sql-mapper';
+import graphqlPlugin from '@platformatic/sql-graphql'
+
+declare module '@platformatic/sql-mapper' {
+  interface Entities {
+    
+  }
+}
